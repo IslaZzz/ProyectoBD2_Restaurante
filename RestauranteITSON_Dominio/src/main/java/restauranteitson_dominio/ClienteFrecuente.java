b4f272a1c4b704eec1066ce83a20c14f52c55e5a
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2025-04-02 15:50:33 -0700

    ClientesFrecuentes entidad

---
+++ RestauranteITSON_Dominio/src/main/java/restauranteitson_dominio/ClienteFrecuente.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restauranteitson_dominio;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ *
+ * @author abrilislas
+ */
+@Entity
+public class ClienteFrecuente extends Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idCliente")
+    private Long id;
+
+    
+    @Column(name="numeroVisitas")
+    private Integer numeroVisitas;
+    
+    @Column(name="fechaUltimaComanda")
+    private Calendar fechaUltimaComanda;
+    
+    @Column(name="totalGastado")
+    private Double totalGastado;
+    
+    @Column(name="puntosFidelidad")
+    private Integer puntosFidelidad;
+    
+    //Constructor por defecto
+    
+    //Getters y Setters
+    
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumeroVisitas() {
+        return numeroVisitas;
+    }
+
+    public void setNumeroVisitas(Integer numeroVisitas) {
+        this.numeroVisitas = numeroVisitas;
+    }
+
+    public Calendar getFechaUltimaComanda() {
+        return fechaUltimaComanda;
+    }
+
+    public void setFechaUltimaComanda(Calendar fechaUltimaComanda) {
+        this.fechaUltimaComanda = fechaUltimaComanda;
+    }
+
+    public Double getTotalGastado() {
+        return totalGastado;
+    }
+
+    public void setTotalGastado(Double totalGastado) {
+        this.totalGastado = totalGastado;
+    }
+
+    public Integer getPuntosFidelidad() {
+        return puntosFidelidad;
+    }
+
+    public void setPuntosFidelidad(Integer puntosFidelidad) {
+        this.puntosFidelidad = puntosFidelidad;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ClienteFrecuente)) {
+            return false;
+        }
+        ClienteFrecuente other = (ClienteFrecuente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "restauranteitson_dominio.ClienteFrecuente[ id=" + id + " ]";
+    }
+    
+}
