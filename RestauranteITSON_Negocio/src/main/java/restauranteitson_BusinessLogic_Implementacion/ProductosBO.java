7a132329500d746907dd67be9d5e6a506cc3f951
sansanty03 <138645645+sansanty03@users.noreply.github.com>
2025-04-07 11:17:35 -0700

    crear productoBO

---
+++ RestauranteITSON_Negocio/src/main/java/restauranteitson_BusinessLogic_Implementacion/ProductosBO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restauranteitson_BusinessLogic_Implementacion;
+
+import java.util.List;
+import restauranteitson_BusinessLogic_Interfaces.IProductosBO;
+import restauranteitson_dominio.Producto;
+
+/**
+ *
+ * @author LABCISCO-PC006
+ */
+public class ProductosBO implements IProductosBO {
+
+    @Override
+    public Producto agregarProducto() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Producto> consultarProductos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ RestauranteITSON_Negocio/src/main/java/restauranteitson_BusinessLogic_Interfaces/IProductosBO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package restauranteitson_BusinessLogic_Interfaces;
+
+import java.util.List;
+import restauranteitson_dominio.Producto;
+
+/**
+ *
+ * @author LABCISCO-PC006
+ */
+public interface IProductosBO {
+    public abstract Producto agregarProducto();
+    
+    public abstract List<Producto> consultarProductos();
+    
+    
+}
